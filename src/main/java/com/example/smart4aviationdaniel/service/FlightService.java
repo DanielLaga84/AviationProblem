b77d5b3800fd79b63abc58c846d1b7e7e8f91e9f
daniellaga84@gmail.com <Erkano84!!>
2021-05-16 14:13:08 +0200

    Clean code and updated.

--- src/main/java/com/example/smart4aviationdaniel/service/FlightService.java
+++ src/main/java/com/example/smart4aviationdaniel/service/FlightService.java
@@ -3,82 +3,66 @@ package com.example.smart4aviationdaniel.service;
 import com.example.smart4aviationdaniel.repository.BaggageRepostitory;
 import com.example.smart4aviationdaniel.repository.CargoRepository;
 import com.example.smart4aviationdaniel.repository.FlightRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
-public class FlightService
-{
-    private FlightRepository flightRepository;
-    private CargoRepository cargoRepository;
-    private BaggageRepostitory baggageRepository;
+public class FlightService {
+    private final FlightRepository flightRepository;
+    private final CargoRepository cargoRepository;
+    private final BaggageRepostitory baggageRepository;
 
-    FlightService (FlightRepository flightRepository, CargoRepository cargoRepository, BaggageRepostitory baggageRepository)
-    {
+    FlightService(FlightRepository flightRepository, CargoRepository cargoRepository, BaggageRepostitory baggageRepository) {
         this.flightRepository = flightRepository;
         this.cargoRepository = cargoRepository;
-        this.baggageRepository=baggageRepository;
+        this.baggageRepository = baggageRepository;
     }
 
-    public double cargoWeight(Integer flightId)
-    {
+    public double cargoWeight(Integer flightId) {
 
         return cargoRepository.findByFlightId(flightId).stream().map(c -> {
-            double weight=c.getWeight();
-            if (c.getWeightUnit().equals("lb"))	weight=weight/2.2046;
-            return c.getPiecesOfCargo()*weight;
+            double weight = c.getWeight();
+            if (c.getWeightUnit().equals("lb")) weight = weight / 2.2046;
+            return c.getPiecesOfCargo() * weight;
         }).collect(Collectors.summingDouble(Double::doubleValue));
 
     }
 
-    public double baggageWeight(Integer flightId)
-    {
+    public double baggageWeight(Integer flightId) {
 
         return baggageRepository.findByFlightId(flightId).stream().map(c -> {
-            double weight=c.getWeight();
-            if (c.getWeightUnit().equals("lb"))	weight=weight/2.2046;
-            return c.getPiecesOfCargo()*weight;
+            double weight = c.getWeight();
+            if (c.getWeightUnit().equals("lb")) weight = weight / 2.2046;
+            return c.getPiecesOfCargo() * weight;
         }).collect(Collectors.summingDouble(Double::doubleValue));
 
     }
 
-    public long numberOfFlightsDepartingFromAirport(LocalDate date, String airportCode)
-    {
-        return flightRepository.findByDepartureDateAndAirportCode(date,airportCode).stream().filter(f-> f.getAirportCodeDeparture().equals(airportCode)).count();
+    public long numberOfFlightsDepartingFromAirport(LocalDate date, String airportCode) {
+        return flightRepository.findByDepartureDateAndAirportCode(date, airportCode).stream().filter(f -> f.getAirportCodeDeparture().equals(airportCode)).count();
     }
 
-    public long numberOfFlightsArrivingToTheAirport(LocalDate date, String airportCode)
-    {
-        return flightRepository.findByDepartureDateAndAirportCode(date,airportCode).stream().filter(f-> f.getAirportCodeArrival().equals(airportCode)).count();
+    public long numberOfFlightsArrivingToTheAirport(LocalDate date, String airportCode) {
+        return flightRepository.findByDepartureDateAndAirportCode(date, airportCode).stream().filter(f -> f.getAirportCodeArrival().equals(airportCode)).count();
     }
 
-    int totalNumberOfBaggageOfOneFlight(Integer flightId)
-    {
-        return baggageRepository.findByFlightId(flightId).stream().map(b-> b.getPiecesOfCargo()).collect(Collectors.summingInt(Short::shortValue));
+    int totalNumberOfBaggageOfOneFlight(Integer flightId) {
+        return baggageRepository.findByFlightId(flightId).stream().map(b -> b.getPiecesOfCargo()).collect(Collectors.summingInt(Short::shortValue));
     }
 
-    public int totalNumberOfBaggageArrivingToTheAirport(LocalDate date, String airportCode)
-    {
+    public int totalNumberOfBaggageArrivingToTheAirport(LocalDate date, String airportCode) {
         return flightRepository.findByDepartureDateAndAirportCode(date, airportCode).stream()
-                .filter(f->f.getAirportCodeArrival().equals(airportCode))
-                .map(f->totalNumberOfBaggageOfOneFlight(f.getId()))
+                .filter(f -> f.getAirportCodeArrival().equals(airportCode))
+                .map(f -> totalNumberOfBaggageOfOneFlight(f.getId()))
                 .collect(Collectors.summingInt(Integer::intValue));
     }
 
-    public int totalNumberOfBaggageDepartingFromTheAirport(LocalDate date, String airportCode)
-    {
+    public int totalNumberOfBaggageDepartingFromTheAirport(LocalDate date, String airportCode) {
         return flightRepository.findByDepartureDateAndAirportCode(date, airportCode).stream()
-                .filter(f->f.getAirportCodeDeparture().equals(airportCode))
-                .map(f->totalNumberOfBaggageOfOneFlight(f.getId()))
+                .filter(f -> f.getAirportCodeDeparture().equals(airportCode))
+                .map(f -> totalNumberOfBaggageOfOneFlight(f.getId()))
                 .collect(Collectors.summingInt(Integer::intValue));
     }
-
-
-
-
 }
