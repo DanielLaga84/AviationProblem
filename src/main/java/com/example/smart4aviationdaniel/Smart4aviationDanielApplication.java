6f92ef85b1d3489bab23dbe2dbd8724b84f2c0e4
daniellaga84@gmail.com <Erkano84!!>
2021-05-16 12:08:37 +0200

    Initial commit

---
+++ src/main/java/com/example/smart4aviationdaniel/Smart4aviationDanielApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart4aviationdaniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smart4aviationDanielApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Smart4aviationDanielApplication.class, args);
+    }
+
+}
